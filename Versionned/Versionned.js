d9606d524c72e3a255a6f8d5a7b6718b749e5b62
David Bruant <bruant.d@gmail.com>
2011-12-14 12:38:32 +0100

    renaming

--- Versionned/Versionned.js
+++ Versionned/Versionned.js
@@ -1,14 +1,33 @@
 "use strict";
 
+function History(){
+    var target = [];
+    target.addEntry = Array.prototype.push.bind(target);
+    
+    var handler = {
+        get: function(target, name){
+            if(this.hasOwn(target, name))
+                return target[i];
+                // TODO: figure out how to better filter
+        }
+    }
+    
+    return Proxy([], handler);
+}
+
+
+
 /*****
-** Replayable
+** Versionned objects
 */
 
 (function(global){
     
     /*** UTILS ***/
     
-    // Test is 2 values are the same
+    /***
+    ** Test if 2 values are the same
+    */
     function is(x, y) {
         if (x === y) {
             // -0 and +0 are different values  
@@ -19,34 +38,30 @@
         return x !== x && y !== y;
     }
     
-    // Tests whether two property descriptors described the same property
+    /***
+    ** Tests whether two property descriptors described the same property
+    */
     function sameDescriptor(pd1, pd2){
+        // TODO: complete the descriptors beforehand?
+    
         return Object.keys(pd1).every(function(attr){
             return is(pd1[attr], pd2[attr]);
         });
     }
     
+    
+    
      // TODO: replace this dummy function
-    function RemoteControl(replayable, history){
+    function Snapshots(initState, history){        
         /*
-        Maybe the replayable can be replaced dynamically?
+        Array-like object. No setters at all
+        
         */
-        
-        
-        {move:function(delta){},
-         go:function(historyIndex){},
-         get length(){
+        {get length(){
             return history.length;
          },
          numericIndexes: "snapshots", // requires to create a proxy
-         get rewind(){
-            this.move(-1);
-            return this;
-         },
-         get forward(){
-            this.move(+1);
-            return this;
-         }
+         latest: {} // live read-only copy of the latest
         };
         
     }
@@ -83,6 +98,9 @@
         delete: function(target, name){
             var currentPd = Object.getOwnPropertyDescriptor(target, name);
             
+            if(!currentPd)
+                return true; 
+            
             // expediates dummy case
             
             // forward operation to the target
@@ -105,20 +123,25 @@
 
 
     /**
-    ** Open question: create a replayable of an
+    ** From an object, creates:
+    ** 1) Another object which actions are going to be recorded but that will
+    ** appear to be exactly the same that the target object with the exception
+    ** of object identity.
+    ** 2) An object which can show the different states the object went through
     */
-    global.Replayable = function(o){
+    global.Versionned = function(toRecord){
         
         var handler = Object.create(handlerProto);
-        handler.history = [];
+        handler.history = new History();
 
         // TODO: fake calls to pE/seal/freeze/is{$1,$2,$3} & Object.defineProperty 
         // in cases they do something irreversible
-        var replayable = new Proxy(o, handler);
-        var remoteControl = new RemoteControl(o, history);
+        // TODO: instead of using toRecord as the target, branch it and use the branch
+        var recorded = new Proxy(toRecord, handler);
+        var snapshots = new Snapshots(recorded, history);
 
-        return {replayable: replayable,
-                remoteControl: remoteControl};
+        return {recorded: recorded,
+                snapshots: snapshots};
     };
 
 })(this);

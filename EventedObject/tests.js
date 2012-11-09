4db0153331465e15cb2da7433cba522065e9b1a1
David Bruant <bruant.d@gmail.com>
2012-11-09 10:35:13 +0000

    Added a bunch of tests which all pass. More to come!

--- EventedObject/tests.js
+++ EventedObject/tests.js
@@ -24,46 +24,115 @@ test('Create an event property, retrieve its descriptor', function(){
     strictEqual(Object.getOwnPropertyDescriptor(o, 'e').event, true);
 });
 
+test('Create an event property, check event API', function(){
+    var o = new EventedObject();
+
+    Object.defineProperty(o, 'e', {event: true, configurable: false});
+
+    var eventProperty = o.e;
+
+    strictEqual(typeof eventProperty.fire, 'function');
+    strictEqual(typeof eventProperty.addListener, 'function');
+    strictEqual(typeof eventProperty.removeListener, 'function');
+    ok( !('revoke' in eventProperty), "Make sure revoke isn't exposed");
+});
+
+
+asyncTest('Add a listener and check it is called with the correct event value', function(){
+    var o = new EventedObject();
+    Object.defineProperty(o, 'e', {event: true, configurable: false});
+
+    var listenerCalled = false;
+
+    o.e.addListener(function(e){
+        listenerCalled = true;
+        strictEqual(e, 37, 'Event value is the correct one');
+    });
+
+    o.e.fire(37);
+
+    ok(!listenerCalled, 'The listener has been called synchronously while it should not');
+    setTimeout(function(){
+        ok(listenerCalled);
+        // hopefully we've waited long enough and event firing was scheduled to run before this function (we have no control over that)
+        start();
+    }, 20);
+});
+
+
+asyncTest('Several listeners are called in the order they have been added', function(){
+    var o = new EventedObject();
+    Object.defineProperty(o, 'e', {event: true, configurable: false});
+
+    var listenerCalls = [];
+
+    o.e.addListener(function(e){
+        listenerCalls.push(0);
+    });
+    o.e.addListener(function(e){
+        listenerCalls.push(1);
+    });
+    o.e.addListener(function(e){
+        listenerCalls.push(2);
+    });
+
+    o.e.fire();
+
+    setTimeout(function(){
+        // hopefully we've waited long enough and event firing was scheduled to run before this function (we have no control over that)
+        listenerCalls.forEach(function(e, i){
+            strictEqual(e, i);
+        });
+        start();
+    }, 20);
+});
+
+
+asyncTest('Removing one listener', function(){
+    var o = new EventedObject();
+    Object.defineProperty(o, 'e', {event: true, configurable: false});
+
+    var l1Called = false;
+    var l2Called = false;
+
+    function l1(e){
+        l1Called = true;
+    }
+    function l2(e){
+        l2Called = true;
+    }
+
+    o.e.addListener(l1);
+    o.e.addListener(l2);
+
+    o.e.removeListener(l1);
+
+    o.e.fire();
+
+    setTimeout(function(){
+        // hopefully we've waited long enough and event firing was scheduled to run before this function (we have no control over that)
+        ok(l2Called);
+        ok(!l1Called);
+        start();
+    }, 20);
+});
+
+// adding twice the same listener ?
+// extracting the fire function
+// deleting the event (if extracted any method should throw)
+// test listener's 'this'
+// What should happen with the set trap?
+// more tests for Object.defineProperty (non/configurable, enumerable, etc.)
+
+/** Inheritance
+ * test 'this' value
+ * make sure listeners added from different objects are called
+ *
+ *
+ */
+
+
 /*
-var o = new EventedObject();
-
-// Add properties like a normal object
-o.a = 1;
-o.b = "yo";
-
-// Add an event
-Object.defineProperty(o, 'e', {configurable: true, enumerable: false, event:true});
-
-// Add listeners
-function f2(msg){
-    console.log("f2", msg);
-    console.log("f2", JSON.stringify(this)); // |this| is bound to o
-}
-function f1(msg){
-    console.log("f1", msg);
-}
-
-o.e.addListener(f1);
-o.e.addListener(f2);
-
-
-// Play with the API
-o.e("This is a message"); // f1, f2 are called
-o.e.removeListener(f2);
-o.e("This is another message"); // Only f1 is called
-
-var fire = o.e;
-fire("A message with the method isolated");
-
-// Remove the event from the object.
-delete o.e;
-
-try{
-    fire("Shouldn't work. Actually, it should throw.");
-}
-catch(e){
-    console.log("Error caught", e);
-}
 
 
 // inheritance

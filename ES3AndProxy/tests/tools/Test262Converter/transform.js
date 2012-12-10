abf06fd4463361e1bce433c78608e8bc267c7ef8
David Bruant <bruant.d@gmail.com>
2012-12-10 18:43:54 +0100

    tranform function draft

--- ES3AndProxy/tests/tools/Test262Converter/transform.js
+++ ES3AndProxy/tests/tools/Test262Converter/transform.js
@@ -1,2 +1,12 @@
 "use strict";
 
+var esprima = require('esprima');
+var escodegen = require('escodegen');
+
+
+// wrap all object, array and function declarations in a function called wrapTestObject
+
+module.exports = function transform(sourceCode){
+    var ast = esprima.parse(sourceCode);
+
+};
